3406b2893e55fadd47e749ed27cc878cff2200f7
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-06-17 22:00:30 -0300

    ClienteRepository e ServicoRepository

--- src/main/java/com/qmclouca/springangularboilerplate/model/repository/ClienteRepository.java
+++ src/main/java/com/qmclouca/springangularboilerplate/model/repository/ClienteRepository.java
@@ -2,8 +2,8 @@ package com.qmclouca.springangularboilerplate.model.repository;
 
 import com.qmclouca.springangularboilerplate.model.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
 
-public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
 }
