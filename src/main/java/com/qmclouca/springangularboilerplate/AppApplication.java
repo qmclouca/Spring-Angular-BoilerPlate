5c0ed222c0cd5a5059c67fba3656354a2e2046a5
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-06-21 08:58:30 -0300

    instalando swagger

--- src/main/java/com/qmclouca/springangularboilerplate/AppApplication.java
+++ src/main/java/com/qmclouca/springangularboilerplate/AppApplication.java
@@ -1,26 +1,11 @@
 package com.qmclouca.springangularboilerplate;
 
-import com.qmclouca.springangularboilerplate.model.entity.Cliente;
-import com.qmclouca.springangularboilerplate.model.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
-
-    @Bean
-    public CommandLineRunner run(@Autowired ClienteRepository repository) {
-        return args -> {
-            Cliente cliente = Cliente.builder().cpf("00000000000").nome("Fulano").build();
-            repository.save(cliente);
-        };
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
-
     }
 }
---
+++ src/main/java/com/qmclouca/springangularboilerplate/GeneralConfigurarion.java
@@ -0,0 +1,28 @@
+package com.qmclouca.springangularboilerplate;
+
+import com.qmclouca.springangularboilerplate.annotations.Development;
+import com.qmclouca.springangularboilerplate.annotations.Homologation;
+import com.qmclouca.springangularboilerplate.annotations.Production;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GeneralConfigurarion {
+
+    @Bean
+    @Development
+    public CommandLineRunner executeDev(){
+        return args -> System.out.println("Rodando em DEV");
+    }
+    @Bean
+    @Homologation
+    public CommandLineRunner executeHTML(){
+        return args -> System.out.println("Rodando em HML");
+    }
+    @Bean
+    @Production
+    public CommandLineRunner executePRD(){
+        return args -> System.out.println("Rodando em PRD");
+    }
+}
